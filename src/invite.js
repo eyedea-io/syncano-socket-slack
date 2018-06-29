f45a89c8a58f93fefcadbbfddf137cf4773dc21c
mkucharz <m@kucharz.net>
2018-06-29 16:02:17 +0200

    fix()

--- src/invite.js
+++ src/invite.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import Syncano from '@syncano/core'
-<<<<<<< HEAD
 import Validator from '@syncano/validate'
 
 export default async (ctx) => {
@@ -22,25 +21,6 @@ export default async (ctx) => {
       return response.json({message: resp.data.error}, 400)
     }
     return response.json({message: 'Successfuly invited!'})
-=======
-
-export default async (ctx) => {
-  const {response} = new Syncano(ctx)
-  const slackUrl = 'https://slack.com/api/users.admin.invite'
-
-  try {
-    const resp = await axios.get(slackUrl, {
-      params: {
-        token: ctx.config.SLACK_API_KEY,
-        email: ctx.args.email,
-        set_active: true
-      }})
-
-    if (!resp.data.ok) {
-      return response.json({message: resp.data.error}, 400)
-    }
-    return response('', 204)
->>>>>>> 5bfb563e2b0644c6292318c94fe5bd60ef1916bf
   } catch (err) {
     return response.json({message: err.message}, 400)
   }
