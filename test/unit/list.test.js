064cd94aaeab47b865bd05de328b0f242b704997
mkucharz <m@kucharz.net>
2018-08-11 11:56:26 +0200

    chore(): adding config

--- test/unit/list.test.js
+++ test/unit/list.test.js
@@ -5,14 +5,16 @@ import {run} from '@syncano/test'
 describe('list', function () {
   const args = {token: 'dummy token'}
   it('successful list', async () => {
+    const members = [{
+      id: 123,
+      name: 'Maciej'
+    }]
+
     mockSlack.users.list = jest.fn()
     mockSlack.users.list.mockImplementationOnce(() =>
       Promise.resolve({
         status: 200,
-        members: [{
-          id: 123,
-          name: 'Maciej'
-        }]
+        members
       })
     )
 
@@ -26,17 +28,18 @@ describe('list', function () {
 
     const result = await run('list', {args})
     expect(result).toHaveProperty('code', 200)
+    expect(result.data).toHaveProperty('members', members)
   })
 
   it('error', async () => {
+    const message = 'Something went wrong!'
     mockSlack.users.list = jest.fn()
     mockSlack.users.list.mockImplementationOnce(() =>
-      Promise.reject(new Error({
-        message: 'Something went wrong!'
-      })
+      Promise.reject(new Error({message})
     ))
 
     const result = await run('list', {args})
     expect(result).toHaveProperty('code', 400)
+    expect(result.data).toHaveProperty('message', message)
   })
 })
